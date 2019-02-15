eab7681aea20889c17ea7be0eabcd081652a6699
hamid <hihamidkz@inbox.ru>
2019-02-15 12:49:24 +0700

    Implemented calculator

---
+++ Lesson1/src/Calc.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Calc {
+    public static final void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        float a = 0;
+        float b = 0;
+        float c = 0;
+        String op;
+
+        System.out.println("Введите действие в формате 'число' 'операция' 'число'");
+
+        if (in.hasNextFloat()) {
+            a = in.nextFloat();
+        } else {
+            System.out.println("Вы ввели не число!");
+            System.exit(0);
+        }
+        op = in.next();
+        if (in.hasNextFloat()) {
+            b = in.nextFloat();
+        } else {
+            System.out.println("Вы ввели не число!");
+            System.exit(0);
+        }
+
+        switch (op) {
+            case "+":
+                c = a + b;
+                break;
+            case "-":
+                c = a - b;
+                break;
+            case "*":
+                c = a * b;
+                break;
+            case "/":
+                c = a / b;
+                break;
+            default:
+                System.out.println("Вы ввели неверный операнд");
+                System.exit(0);
+        }
+        System.out.println(c);
+
+    }
+}
