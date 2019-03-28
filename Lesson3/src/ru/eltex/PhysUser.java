414e7d6452d91019aad741dc56136fae46104ac3
hamid <hihamidkz@inbox.ru>
2019-03-28 12:33:21 +0700

    Implemented class PhysUser extending class User

---
+++ Lesson3/src/ru/eltex/PhysUser.java
@@ -0,0 +1,40 @@
+package ru.eltex;
+
+public class PhysUser extends User {
+    private String address;
+
+    public PhysUser() {
+        super();
+    }
+
+    public PhysUser(int id) {
+        super(id);
+    }
+
+    public PhysUser(int id, String FIO, String phone, String address) {
+        super(id, FIO, phone);
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toCSV() {
+        return this.id + ";" + this.FIO + ";" + this.phone + ";" + this.address + "\n";
+    }
+
+    @Override
+    public void fromCSV(String csv) {
+        String[] arr = csv.split(";");
+        this.id = Integer.valueOf(arr[0]);
+        this.FIO = arr[1];
+        this.phone = arr[2];
+        this.address = arr[3];
+    }
+}
