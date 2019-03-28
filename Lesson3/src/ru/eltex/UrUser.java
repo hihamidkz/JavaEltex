b26f644a6d7aa56cb5bc4b84b505a1c99d561203
hamid <hihamidkz@inbox.ru>
2019-03-28 12:39:57 +0700

    Implemented class UrUser extending class User

---
+++ Lesson3/src/ru/eltex/UrUser.java
@@ -0,0 +1,40 @@
+package ru.eltex;
+
+public final class UrUser extends User {
+    private String registrationDate;
+
+    public UrUser() {
+        super();
+    }
+
+    public UrUser(int id) {
+        super(id);
+    }
+
+    public UrUser(int id, String FIO, String phone, String registrationDate) {
+        super(id, FIO, phone);
+        this.registrationDate = registrationDate;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Override
+    public String toCSV() {
+        return this.id + "; " + this.FIO + "; " + this.phone + "; " + this.registrationDate + "\n";
+    }
+
+    @Override
+    public void fromCSV(String csv) {
+        String[] arr = csv.split(";");
+        this.id = Integer.valueOf(arr[0]);
+        this.FIO = arr[1];
+        this.phone = arr[2];
+        this.registrationDate = arr[3];
+    }
+}
